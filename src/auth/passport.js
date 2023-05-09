4e77ab02259ac3ffc12921d44f60248f4c4b9754
Lautaax <lautaa_97@outlook.com>
2023-05-08 22:00:14 -0300

    Cambios varios

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -4,45 +4,46 @@ import userModel from "../dao/models/user.model.js";
 import { createHash, isValidPassword } from "../utils.js";
 import GithubStrategy from "passport-github2"
 import config from "../config.js";
-import { cartModel } from "../dao/models/cart.model.js";
 
 
 const { clientID, clientSecret, callbackUrl } = config
 const LocalStrategy = local.Strategy
 const initializePassport = () => {
-    passport.use("register", new LocalStrategy({ 
-        passReqToCallback: true, 
-        usernameField: "email" },
-        async (req, username, password, done) => {
-        try {
-            const { first_name, last_name, email, role,age } = req.body;
-            let user = await userModel.findOne({ email: username });
-            if (user) {
-                return done(null, false);
-            }
+    passport.use(
+        "register",
+        new LocalStrategy(
+            {
+                passReqToCallback: true,
+                usernameField: "email",
+            },
+            async (req, username, password, done) => {
+                try {
+                    const { first_name, last_name, email, role, age } = req.body;
+                    let user = await userModel.findOne({ email: username });
+                    if (user) {
+                        console.log("User already exists");
+                        return done(null, false);
+                    }
+                    const cart = await cartModel.create({});
 
-            if (userExist){
-                console.log("Usuario ya creado");
-                done (null, false);
-            }
-            const cart = await cartModel.create ({})
+                    const newUser = {
+                        first_name,
+                        last_name,
+                        email,
+                        age,
+                        role: role ?? "user",
+                        password: createHash(password),
+                        cart: cart._id,
+                    };
 
-            const newUser = {
-                first_name,
-                last_name,
-                email,
-                age,
-                role: role ?? "user",
-                password: createHash(password),
-                cartId: cart._id,
+                    const result = await userModel.create(newUser);
+                    return done(null, result);
+                } catch (error) {
+                    return done("Error on trying to find user" + error);
+                }
             }
-
-            const result = await userModel.create(newUser);
-            return done(null, result);
-        } catch (error) {
-            return done("Error on trying to find user" + error);
-        }
-    }));
+        )
+    );
     passport.use("login", new LocalStrategy({ usernameField: "email" }, async (username, password, done) => {
         try {
 
@@ -64,40 +65,46 @@ const initializePassport = () => {
         }
     }));
 
-    passport.use("githublogin", new GithubStrategy({
+    passport.use("githublogin", 
+    new GithubStrategy({
         clientID,
         clientSecret,
-        callbackUrl
-    }, async (accessToker, refreshToken, profile, done) => {
+        callbackUrl,
+      },
+      async (accessToken, refreshToken, profile, done) => {
         try {
             console.log(profile)
-            let user = await userModel.findOne({ email: profile._json.email }).lean()
-            if(user.email === "lautaa_97@outlook.com"){
-                user.role = "admin"
-            }
-            if (user.email === "adminCoder@coder.com") {
-                user.role = "admin"
-            } else {
-                user.role = "user"
-            }
-            delete user.password
-            if (!user) {
-                let newUser = {
-                    first_name: profile._json.name,
-                    last_name: "",
-                    age: 18,
-                    email: profile._json.email,
-                    password: "",
+          let user = await userModel.findOne({ email: profile._json.email }).lean();
 
-                }
-                let result = await userModel.create(newUser)
-                return done(null, result)
-            }
-            return done(null, user)
-        } catch (error) {
-            return done(error)
+          if(user.email === "lautaa_97@outlook.com"){
+            user.role = "admin"
         }
-    }))
+        if (user.email === "adminCoder@coder.com") {
+            user.role = "admin"
+        } else {
+            user.role = "user"
+        }
+        delete user.password
+          if (!user) {
+            let newUser = {
+              first_name: profile._json.name,
+              last_name: "",
+              age: 18,
+              email: profile._json.email,
+              password: "",
+            };
+
+            let result = await userModel.create(newUser);
+            return done(null, result);
+          }
+
+          return done(null, user);
+        } catch (error) {
+          return done(error);
+        }
+      }
+    )
+  );
     passport.serializeUser((user, done) => {
 
         done(null, user._id)
